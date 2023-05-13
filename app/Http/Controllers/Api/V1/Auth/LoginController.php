1670899d4048d0273b1d34cbdc6d7abd5fb7346b
Chimuka Moonde <chimukamoonde@gmail.com>
2023-05-13 18:24:22 +0200

    minor adjustments

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -18,27 +18,68 @@ class LoginController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
+        try {
 
-        $user = User::where('email', $request->email)->first();
+            $validateUser = Validator::make($request->all(),
+                [
+                    'man_no' => ['required'],
+                    'password' => ['required'],
+                ]
+            );
 
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+            if ($validateUser->fails()) {
+                return response()->json(
+                    [
+                        'status' => false,
+                        'message' => 'validation error',
+                        'errors' => $validateUser->errors()
+                    ],
+                    401
+                );
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+//            throw ValidationException::withMessages([
+//                'email' => ['The provided credentials are incorrect.'],
+//            ]);
+                return response()->json(
+                    [
+                        'message' => 'The provided credentials are incorrect'
+                    ],
+                    401
+                );
+            }
+
+            $device = substr($request->userAgent() ?? '', 0, 255);
+
+            //we implement the "remember me" functionality: if the $request->remember is present and true,
+            // then we set the additional expiresAt parameter in the Sanctum createToken() method.
+            $expiresAt = $request->remember ? null : now()->addMinutes(config('session.lifetime'));
+
+
+            return response()->json(
+                [
+                    'status' => true,
+                    'message' => 'User Logged in successfully',
+                    'token_type' => 'Bearer',
+//                    'access_token' => $user->createToken($device, expiresAt: $expiresAt)->plainTextToken,
+                    'access_token' => $user->createToken($device)->plainTextToken,
+                ],
+                200
+            );
+
+
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    'status' => false,
+                    'message' => $th->getMessage()
+                ],
+                500
+            );
         }
 
-        $device = substr($request->userAgent() ?? '', 0, 255);
-
-        //we implement the "remember me" functionality: if the $request->remember is present and true,
-        // then we set the additional expiresAt parameter in the Sanctum createToken() method.
-        $expiresAt = $request->remember ? null : now()->addMinutes(config('session.lifetime'));
-
-        return response()->json([
-            'access_token' => $user->createToken($device, expiresAt: $expiresAt)->plainTextToken,
-        ], Response::HTTP_CREATED);
     }
 }
--- app/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -19,27 +19,59 @@ class RegisterController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $request->validate([
-            'name'     => ['required', 'string', 'max:255'],
-            'email'    => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Password::defaults()],
-        ]);
 
-        $user = User::create([
-            'name'     => $request->name,
-            'email'    => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        try {
 
-        //we're firing a general Laravel Auth event, that could be caught with any Listeners in the future
-        event(new Registered($user));
+            $validateUser = Validator::make($request->all(),
+                [
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                    'password' => ['required', 'confirmed', Password::defaults()],
+                ]
+            );
 
-        //we have a $device variable, coming automatically from the User Agent,
-        // so we're creating a token specifically for that front-end device, like a mobile phone
-        $device = substr($request->userAgent() ?? '', 0, 255);
+            if ($validateUser->fails()) {
+                return response()->json(
+                    [
+                        'status' => false,
+                        'message' => 'Validation error',
+                        'errors' => $validateUser->errors()
+                    ],
+                    401
+                );
+            }
 
-        return response()->json([
-            'access_token' => $user->createToken($device)->plainTextToken,
-        ], Response::HTTP_CREATED);
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            //we're firing a general Laravel Auth event, that could be caught with any Listeners in the future
+            event(new Registered($user));
+
+            //we have a $device variable, coming automatically from the User Agent,
+            // so we're creating a token specifically for that front-end device, like a mobile phone
+            $device = substr($request->userAgent() ?? '', 0, 255);
+
+            return response()->json(
+                [
+                    'status' => true,
+                    'message' => 'User registered successfully',
+                    'token_type' => 'Bearer',
+                    'access_token' => $user->createToken($device)->plainTextToken,
+                ],
+                Response::HTTP_CREATED
+            );
+
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    'status' => false,
+                    'message' => $th->getMessage()
+                ],
+                500
+            );
+        }
     }
 }
